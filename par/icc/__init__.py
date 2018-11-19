b0220af6e445c62be72e6fd07691eed42913e386
Edward Wang <edward.c.wang@compdigitec.com>
2018-11-19 00:37:20 -0800

    Adapt to latest hammer changes

--- par/icc/__init__.py
+++ par/icc/__init__.py
@@ -10,10 +10,11 @@ from shutil import copyfile
 from os.path import dirname
 from typing import List
 
+import hammer_tech
 from hammer_vlsi import HammerPlaceAndRouteTool, HammerToolStep
 from hammer_vlsi import SynopsysTool
 from hammer_logging import HammerVLSILogging
-
+from hammer_tech import HammerTechnologyUtils
 
 class ICC(HammerPlaceAndRouteTool, SynopsysTool):
     def fill_outputs(self) -> bool:
@@ -86,11 +87,11 @@ export SNPSLMD_LICENSE_FILE="{snps}"
 #~ fi
 
         # Gather/load libraries.
-        timing_dbs = ' '.join(self.read_libs([self.timing_db_filter], self.to_plain_item))
-        milkyway_lib_dirs = ' '.join(self.read_libs([self.milkyway_lib_dir_filter], self.to_plain_item))
-        milkyway_techfiles = ' '.join(self.read_libs([self.milkyway_techfile_filter], self.to_plain_item))
-        tlu_max_caps = ' '.join(self.read_libs([self.tlu_max_cap_filter], self.to_plain_item))
-        tlu_min_caps = ' '.join(self.read_libs([self.tlu_min_cap_filter], self.to_plain_item))
+        timing_dbs = ' '.join(self.technology.read_libs([hammer_tech.filters.timing_db_filter], HammerTechnologyUtils.to_plain_item))
+        milkyway_lib_dirs = ' '.join(self.technology.read_libs([hammer_tech.filters.milkyway_lib_dir_filter], HammerTechnologyUtils.to_plain_item))
+        milkyway_techfiles = ' '.join(self.technology.read_libs([hammer_tech.filters.milkyway_techfile_filter], HammerTechnologyUtils.to_plain_item))
+        tlu_max_caps = ' '.join(self.technology.read_libs([hammer_tech.filters.tlu_max_cap_filter], HammerTechnologyUtils.to_plain_item))
+        tlu_min_caps = ' '.join(self.technology.read_libs([hammer_tech.filters.tlu_min_cap_filter], HammerTechnologyUtils.to_plain_item))
 
         if timing_dbs == "":
             self.logger.error("No timing dbs (libs) specified!")
--- synthesis/dc/__init__.py
+++ synthesis/dc/__init__.py
@@ -10,6 +10,8 @@ from typing import List, Optional
 from hammer_vlsi import HammerSynthesisTool, HammerToolStep
 from hammer_vlsi import SynopsysTool
 from hammer_logging import HammerVLSILogging
+import hammer_tech
+from hammer_tech import HammerTechnologyUtils
 
 import os
 import re
@@ -106,9 +108,9 @@ class DC(HammerSynthesisTool, SynopsysTool):
         input_files = list(self.input_files)  # type: List[str]
         # Add any verilog_synth wrappers (which are needed in some technologies e.g. for SRAMs) which need to be
         # synthesized.
-        input_files += self.read_libs([
-            self.verilog_synth_filter
-        ], self.to_plain_item)
+        input_files += self.technology.read_libs([
+            hammer_tech.filters.verilog_synth_filter
+        ], HammerTechnologyUtils.to_plain_item)
 
         # Generate preferred_routing_directions.
         preferred_routing_directions_fragment = os.path.join(self.run_dir, "preferred_routing_directions.tcl")
@@ -121,14 +123,14 @@ class DC(HammerSynthesisTool, SynopsysTool):
             f.write(self.sdc_clock_constraints)
 
         # Get libraries.
-        lib_args = self.read_libs([
-            self.timing_db_filter._replace(tag="lib"),
-            self.milkyway_lib_dir_filter._replace(tag="milkyway"),
-            self.tlu_max_cap_filter._replace(tag="tlu_max"),
-            self.tlu_min_cap_filter._replace(tag="tlu_min"),
-            self.tlu_map_file_filter._replace(tag="tlu_map"),
-            self.milkyway_techfile_filter._replace(tag="tf")
-        ], self.to_command_line_args)
+        lib_args = self.technology.read_libs([
+            hammer_tech.filters.timing_db_filter._replace(tag="lib"),
+            hammer_tech.filters.milkyway_lib_dir_filter._replace(tag="milkyway"),
+            hammer_tech.filters.tlu_max_cap_filter._replace(tag="tlu_max"),
+            hammer_tech.filters.tlu_min_cap_filter._replace(tag="tlu_min"),
+            hammer_tech.filters.tlu_map_file_filter._replace(tag="tlu_map"),
+            hammer_tech.filters.milkyway_techfile_filter._replace(tag="tf")
+        ], HammerTechnologyUtils.to_command_line_args)
 
         # Pre-extract the tarball (so that we can make TCL modifications in Python)
         self.run_executable([
